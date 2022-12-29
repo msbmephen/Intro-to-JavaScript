312d25e09f5927e3c7fc280b872f8fc84326d377
msbmephen <121531061+msbmephen@users.noreply.github.com>
2022-12-29 11:53:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function greet (name, height, country){
-    alert ('Hello ' + name + 'My height is' + height +'I live in the' + country);
+    alert ('Hello ' + name + 'My height is' + height +'I live in the ' + country);
 }
-    greet ('Marlena. ', '5 ft, 0 in. ', 'USA');
+    greet ('Marlena. ', '5 ft, 0 in. ', 'USA.');
